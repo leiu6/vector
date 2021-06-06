6a4672b94cb88f547a7aefbb752880bfe5a56d9b
Leighton Mitchell <ltmitchell2000@gmail.com>
2021-06-06 11:18:12 -0500

    Added a function to tell if a triangle is equilateral in shape.h

--- main.c
+++ main.c
@@ -1,23 +1,8 @@
 #include <stdio.h>
 #include <math.h>
-#include "vector.h"
 #include "shape.h"
 
 int main() {
-    vect a, b;
-
-    a.x = 1;
-    a.y = 0;
-    a.z = 0;
-
-    b.x = 0;
-    b.y = 1;
-    b.z = 0;
-
-    float angle = angl(a, b);
-
-    angle *= 180 / 3.1415;
-
-    printf("%f\n", angle);
+    tri test;
     return 0;
 }
--- shape.h
+++ shape.h
@@ -1,5 +1,34 @@
+#include "vector.h"
+
 typedef struct {
     vect v1;
     vect v2;
     vect v3;
 } tri;
+
+int isEquilateral (tri input) {
+    vect s1, s2, s3;
+    
+    s1.x = input.v1.x - input.v2.x;
+    s1.y = input.v1.y - input.v2.y;
+    s1.z = input.v1.z - input.v2.z;
+    
+    s2.x = input.v2.x - input.v3.x;
+    s2.y = input.v2.y - input.v3.y;
+    s2.z = input.v2.z - input.v3.z;
+    
+    s3.x = input.v3.x - input.v1.x;
+    s3.y = input.v3.y - input.v1.y;
+    s3.z = input.v3.z - input.v1.z;
+    
+    float angleV1 = angl(s1, s3);
+    float angleV2 = angl(s1, s2);
+    float angleV3 = angl(s2, s3);
+    
+    if ((angleV1 == angleV2 && angleV2 == angleV3) && angleV1 == angleV3) {
+        return 1;
+    }
+    else {
+        return 0;
+    }
+}
